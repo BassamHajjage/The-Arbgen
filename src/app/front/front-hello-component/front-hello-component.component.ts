9c47343ddc248336def398a9e44b3b51c638d9e9
laterfinancial <legal.latr@gmail.com>
2021-08-17 01:51:47 -0400

    1.0.5

--- src/app/front/front-hello-component/front-hello-component.component.ts
+++ src/app/front/front-hello-component/front-hello-component.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from "rxjs";
-import { nanoid } from 'nanoid'
 
 @Component({
   selector: 'app-front-hello-component',
@@ -10,19 +9,31 @@ import { nanoid } from 'nanoid'
 export class FrontHelloComponentComponent implements OnInit {
 
   code = new BehaviorSubject<string>("");
+  sequence = "CTCCTATAACGAACTTCGACATGGCAAAATCCCCCCCTCGCGACTTCTAGAGAAGAAGAGTACTGACTTGAGCGCTCCCAGCACTTCAGCCAAGGAAGTTACCAATTTCTTGTTTCCGAATGACACGCGTCTCCTTGCGGGTAAATCGCCGACCGCAGAACTTACGAGCCAGGGGAAACAGTAAGGCCTAATTAGGTAAAGGGAGTAAGTGCTCGAACGGTTCAGTTGTAACCATATACTTACGCTGGATCTTCTCCGGCGAATTTTTACCGTCACCAACTACGAGATTTGAGGTAAACCAAATGAGCACATAGTGGCGCTATCCGACTATTTCCAAATTGTAACATATCGTTCCATGAAGGCCAGAGTTACTTACCGGCCCTTTCCATGCGCGCGCCATACCCTCCTAGTTCCCCGGTTATCTCTCCGAGGAGAGAGTGAGCGATCCTCCGTTAACATATTGTTACCAATGACGTAGCTATGTATTTTGCACAGGTAGCCAACGGGTTTCACATTTCACAGATAGTGGGGATCCCGGCAAAGGGCGTATATTTGCGGTCCAACATAGGCGTAAACTACGATGGCACCTACTCAGACGCAGCTCGTGCGGCGTAAATAACGTACTCATCCCAACTGATTCTCGGCAATCTACGGAGCGACATGATTATCAACAGCTGTCTAGCAGTTCTAATCTTTTGCCATGGTCGTAAAAGCCTCCAAGAGATTGATCATACCTATCGGCACAGAAGTGACACGACGCCGATGGGTAGCGGACTTTAGGTCAACCACAGTTCGGTAGGGGACAGGCCCTGCGGCGTACATCACTTTGTATGTGCAACGTGCCCAAGTGGCGCCAGGCAAGACTCAGCTGGTTCCTGTGTTAGCTCGAGGCTAGGCATGACAGCTCTTTGAACATGGGCTGGGGGCCTCGAACGGTCGAGAAGCCCATAGTACCTCGGATACCAAGTTGCGCAGGCTATAGCTTGAAGCTGTACTATTT";
+  sequenceLength = this.sequence.length;
 
   constructor(
-    // private clipboard: Clipboard
   ) {
     this.code.subscribe(val => {
-      console.log("val: " + val)
-      if (val.length === 0) {
-      }
+      const randomStart = Math.floor((Math.random()*this.sequenceLength) + 1);
+      console.log(randomStart);
+      const uh124 = this.sequence.substring(randomStart, randomStart + 4);
+      console.log("Val: " + uh124 + "-" + val);
     })
   }
 
   ngOnInit(): void {
     this.createCode();
+    console.log(this.randomString(10))
+    console.log(this.sequenceLength)
+  }
+  randomString(length: number) {
+    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
   }
 
   copyCode() {
@@ -30,7 +41,7 @@ export class FrontHelloComponentComponent implements OnInit {
     this.createCode();
   }
   createCode(config?: {}) {
-    const newNanoid = nanoid(20);
+    const newNanoid = this.randomString(7);
     this.code.next(newNanoid);
   }
   isNumeric(num: any) {
